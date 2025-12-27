2604c220d03fd32aec493dff96309bc24eba41dd
Lakksithharikarboncard <lakksith.hariie@karboncard.com>
2025-12-27 13:34:59 +0000

    clean version 1.1

--- linkedinresponder/New folder/src/shared/utils.ts
+++
@@ -1,6 +0,0 @@
-export function randomBetween(min: number, max: number): number {
-  return Math.floor(min + Math.random() * (max - min + 1));
-}
-export function sleep(ms: number): Promise<void> {
-  return new Promise((res) => setTimeout(res, ms));
-}
